ffd5edb8bcf56bf0d9fd14130b7388b6ed3127a5
JasonZhang <2403501171@qq.com>
2020-03-19 20:28:47 +0800

    fix dataformat

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,13 +4,13 @@ import BraftEditor from 'braft-editor'
 export const dataFormat = (obj, rules, reverse) => {
   rules.forEach(rule => {
     const { key, type, format } = rule
-    if (type === 'html') {
+    if (type === 'html' && obj[key]) {
       obj[key] = !reverse ? obj[key].toHTML() : BraftEditor.createEditorState(obj[key])
     }
-    if (type === 'moment') {
+    if (type === 'moment' && obj[key]) {
       obj[key] = !reverse ? obj[key].format(format ? format : 'YYYY-MM-DD') : moment(obj[key])
     }
-    if (type === 'color') {
+    if (type === 'color' && obj[key]) {
       obj[key] = !reverse ? obj[key].hex : obj[key]
     }
   })
